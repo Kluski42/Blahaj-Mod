cee3526371115e747f724e9c96596926c5b192d5
Ashlyn <kluski42@gmail.com>
2025-04-17 00:05:55 -0400

    Start

---
+++ src/client/java/net/wetnoodle/blahaj/BlahajClient.java
@@ -0,0 +1,10 @@
+package net.wetnoodle.blahaj;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlahajClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
