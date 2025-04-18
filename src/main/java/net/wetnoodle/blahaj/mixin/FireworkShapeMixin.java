e77edab819e24f64254915c616de7871b6be68aa
Wet Noodle <kluski42@gmail.com>
2025-04-18 16:36:52 -0400

    Added crafting recipe for fireworks

--- src/main/java/net/wetnoodle/blahaj/mixin/FireworkShapeMixin.java
+++ src/main/java/net/wetnoodle/blahaj/mixin/FireworkShapeMixin.java
@@ -17,33 +17,30 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @Mixin(FireworkExplosion.Shape.class)
-public class FireworkShapeMixin  {
+public class FireworkShapeMixin {
 
-   @Final
-   @Shadow
-   @Mutable
-   private static Shape[] $VALUES;
+    @Final
+    @Shadow
+    @Mutable
+    private static Shape[] $VALUES;
 
-   @SuppressWarnings("InvokerTarget")
-   @Invoker("<init>")
-   private static Shape blahaj$newShape(String internalName, int internalId, int id, String name) {
-       throw new AssertionError("Mixin injection failed - Blahaj FireworkShapeMixin.");
-   }
+    @Invoker("<init>")
+    private static Shape blahaj$newShape(String internalName, int internalId, int id, String name) {
+        throw new AssertionError("Mixin injection failed - Blahaj FireworkShapeMixin.");
+    }
 
+    @Inject(method = "<clinit>", at = @At(value = "FIELD",
+            opcode = Opcodes.PUTSTATIC,
+            target = "Lnet/minecraft/world/item/component/FireworkExplosion$Shape;$VALUES:[Lnet/minecraft/world/item/component/FireworkExplosion$Shape;",
+            shift = At.Shift.AFTER))
+    private static void blahaj$addCustomShape(CallbackInfo info) {
+        var types = new ArrayList<>(Arrays.asList($VALUES));
+        var last = types.getLast();
 
-   @Inject(method = "<clinit>", at = @At(value = "FIELD",
-           opcode = Opcodes.PUTSTATIC,
-           target = "Lnet/minecraft/world/item/component/FireworkExplosion$Shape;$VALUES:[Lnet/minecraft/world/item/component/FireworkExplosion$Shape;",
-           shift = At.Shift.AFTER))
-   private static void blahaj$addCustomShape(CallbackInfo info) {
-      var types = new ArrayList<>(Arrays.asList($VALUES));
-      var last = types.getLast();
+        Shape blahaj = blahaj$newShape("BLAHAJBLAHAJ", last.ordinal() + 1, last.ordinal() + 1, "blahajblahaj");
+        BlFireworkShapeTypes.BLAHAJ = blahaj;
+        types.add(blahaj);
 
-
-      Shape blahaj = blahaj$newShape("BLAHAJBLAHAJ", last.ordinal() + 1, last.ordinal() + 1, "blahajblahaj");
-      BlFireworkShapeTypes.BLAHAJ = blahaj;
-      types.add(blahaj);
-
-      $VALUES = types.toArray(new Shape[0]);
-   }
+        $VALUES = types.toArray(new Shape[0]);
+    }
 }
